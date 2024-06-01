41e0d1fac97b16638353c0949059a352c03267d7
Miqael-UG <pmbansah@st.ug.edu.gh>
2024-06-01 18:00:16 +0000

    Add data dir with categories and tasks array

---
+++ data/data.js
@@ -0,0 +1,56 @@
+export const categories = [
+  {
+    name: "Fitness",
+    tasks: "8 Tasks",
+    image: require("../assets/fitness.png"),
+  },
+  {
+    name: "Gardening",
+    tasks: "3 Tasks",
+    image: require("../assets/gardening.png"),
+  },
+  {
+    name: "Programming",
+    tasks: "5 Tasks",
+    image: require("../assets/programming.png"),
+  },
+  { name: "Music", tasks: "2 Tasks", image: require("../assets/music.png") },
+  {
+    name: "Cooking",
+    tasks: "6 Tasks",
+    image: require("../assets/cooking.png"),
+  },
+  {
+    name: "Travel",
+    tasks: "2 Tasks",
+    image: require("../assets/travel.png"),
+  },
+  {
+    name: "Maintenance",
+    tasks: "3 Tasks",
+    image: require("../assets/maintenance.png"),
+  },
+  {
+    name: "Shopping",
+    tasks: "4 Tasks",
+    image: require("../assets/shopping.png"),
+  },
+];
+
+export const tasks = [
+  "Fitness - Morning yoga session",
+  "Fitness - Evening bike ride",
+  "Gardening - Water plants",
+  "Gardening - Trim hedges",
+  "Programming - Review pull requests",
+  "Programming - Code review",
+  "Cooking - Make breakfast",
+  "Cooking - Prepare dinner",
+  "Travel - Book flight tickets",
+  "Travel - Pack luggage",
+  "Shopping - Buy fruits",
+  "Shopping - Get new clothes",
+  "Maintenance - Fix the sink",
+  "Maintenance - Paint the fence",
+  "Music - Practice guitar",
+];
