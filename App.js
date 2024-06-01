e50db98d96298e6e2982c7543a2ab6fb7826e35f
Miqael-UG <pmbansah@st.ug.edu.gh>
2024-06-01 18:17:41 +0000

    - Add header functon - Put header function into SafeAreaView -Style header - Fix bugs

--- App.js
+++ App.js
@@ -10,4 +10,52 @@ import {
   FlatList,
 } from "react-native";
 import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
-import { categories, tasks } from "./data";
+import { categories, tasks } from "./data/data";
+
+const Header = () => (
+  <View style={styles.header}>
+    <View>
+      <Text style={styles.greeting}>Hello, Devs</Text>
+      <Text style={styles.taskInfo}>{tasks.length} tasks today</Text>
+    </View>
+    <View style={styles.userIcon}>
+      <Image source={require("./assets/person.png")} />
+    </View>
+  </View>
+);
+
+const App = () => {
+  return (
+    <SafeAreaView style={styles.container}>
+      <Header />
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#E8F7F0",
+    padding: 20,
+  },
+  header: {
+    marginTop: 40,
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  userIcon: {
+    backgroundColor: "#FFF9FB",
+    borderRadius: 50,
+    padding: 2,
+  },
+  greeting: {
+    fontSize: 32,
+    fontWeight: "bold",
+  },
+  taskInfo: {
+    fontSize: 14,
+    color: "#666",
+  },
+});
+
+export default App;
