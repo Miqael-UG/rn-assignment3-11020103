752090f235fd9a2ba001866ea73993a2b2adf38a
Miqael-UG <pmbansah@st.ug.edu.gh>
2024-06-01 18:21:22 +0000

    - Add searchBar - import needed icons - Style all components in searchBar

--- App.js
+++ App.js
@@ -11,6 +11,10 @@ import {
 } from "react-native";
 import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
 import { categories, tasks } from "./data/data";
+import {
+  faMagnifyingGlass,
+  faSliders,
+} from "@fortawesome/free-solid-svg-icons";
 
 const Header = () => (
   <View style={styles.header}>
@@ -24,10 +28,23 @@ const Header = () => (
   </View>
 );
 
+const SearchBar = () => (
+  <View style={styles.searchBar}>
+    <View style={styles.searchInput}>
+      <FontAwesomeIcon size={20} icon={faMagnifyingGlass} />
+      <TextInput style={styles.input} placeholder="Search" />
+    </View>
+    <View style={styles.filter}>
+      <FontAwesomeIcon size={30} color="#fff" icon={faSliders} />
+    </View>
+  </View>
+);
+
 const App = () => {
   return (
     <SafeAreaView style={styles.container}>
       <Header />
+      <SearchBar />
     </SafeAreaView>
   );
 };
@@ -55,6 +72,31 @@ const styles = StyleSheet.create({
   taskInfo: {
     fontSize: 14,
     color: "#666",
+  },
+  searchBar: {
+    marginTop: 15,
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  searchInput: {
+    flex: 1,
+    flexDirection: "row",
+    alignItems: "center",
+    backgroundColor: "#FFF9FB",
+    borderRadius: 10,
+    paddingLeft: 10,
+    paddingRight: 10,
+    height: 50,
+  },
+  input: {
+    marginLeft: 10,
+    fontSize: 18,
+  },
+  filter: {
+    marginLeft: 10,
+    backgroundColor: "#F0522F",
+    borderRadius: 10,
+    padding: 10,
   },
 });
 
